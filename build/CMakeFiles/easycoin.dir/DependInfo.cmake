c60c59497a44ac35a99b8e2bb7eb3d594c9579d5
Diagon2 <36744384+Diagon2@users.noreply.github.com>
2018-02-24 15:09:02 +0100

    Add files via upload

---
+++ build/CMakeFiles/easycoin.dir/DependInfo.cmake
@@ -0,0 +1,74 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/levi/Desktop/Test/cryptonotewallet/build/easycoin_automoc.cpp" "/home/levi/Desktop/Test/cryptonotewallet/build/CMakeFiles/easycoin.dir/easycoin_automoc.cpp.o"
+  "/home/levi/Desktop/Test/cryptonotewallet/build/qrc_resources.cpp" "/home/levi/Desktop/Test/cryptonotewallet/build/CMakeFiles/easycoin.dir/qrc_resources.cpp.o"
+  "/home/levi/Desktop/Test/cryptonotewallet/src/CommandLineParser.cpp" "/home/levi/Desktop/Test/cryptonotewallet/build/CMakeFiles/easycoin.dir/src/CommandLineParser.cpp.o"
+  "/home/levi/Desktop/Test/cryptonotewallet/src/CryptoNoteWrapper.cpp" "/home/levi/Desktop/Test/cryptonotewallet/build/CMakeFiles/easycoin.dir/src/CryptoNoteWrapper.cpp.o"
+  "/home/levi/Desktop/Test/cryptonotewallet/src/CurrencyAdapter.cpp" "/home/levi/Desktop/Test/cryptonotewallet/build/CMakeFiles/easycoin.dir/src/CurrencyAdapter.cpp.o"
+  "/home/levi/Desktop/Test/cryptonotewallet/src/LoggerAdapter.cpp" "/home/levi/Desktop/Test/cryptonotewallet/build/CMakeFiles/easycoin.dir/src/LoggerAdapter.cpp.o"
+  "/home/levi/Desktop/Test/cryptonotewallet/src/NodeAdapter.cpp" "/home/levi/Desktop/Test/cryptonotewallet/build/CMakeFiles/easycoin.dir/src/NodeAdapter.cpp.o"
+  "/home/levi/Desktop/Test/cryptonotewallet/src/Settings.cpp" "/home/levi/Desktop/Test/cryptonotewallet/build/CMakeFiles/easycoin.dir/src/Settings.cpp.o"
+  "/home/levi/Desktop/Test/cryptonotewallet/src/SignalHandler.cpp" "/home/levi/Desktop/Test/cryptonotewallet/build/CMakeFiles/easycoin.dir/src/SignalHandler.cpp.o"
+  "/home/levi/Desktop/Test/cryptonotewallet/src/WalletAdapter.cpp" "/home/levi/Desktop/Test/cryptonotewallet/build/CMakeFiles/easycoin.dir/src/WalletAdapter.cpp.o"
+  "/home/levi/Desktop/Test/cryptonotewallet/src/gui/AboutDialog.cpp" "/home/levi/Desktop/Test/cryptonotewallet/build/CMakeFiles/easycoin.dir/src/gui/AboutDialog.cpp.o"
+  "/home/levi/Desktop/Test/cryptonotewallet/src/gui/AddressBookDialog.cpp" "/home/levi/Desktop/Test/cryptonotewallet/build/CMakeFiles/easycoin.dir/src/gui/AddressBookDialog.cpp.o"
+  "/home/levi/Desktop/Test/cryptonotewallet/src/gui/AddressBookFrame.cpp" "/home/levi/Desktop/Test/cryptonotewallet/build/CMakeFiles/easycoin.dir/src/gui/AddressBookFrame.cpp.o"
+  "/home/levi/Desktop/Test/cryptonotewallet/src/gui/AddressBookModel.cpp" "/home/levi/Desktop/Test/cryptonotewallet/build/CMakeFiles/easycoin.dir/src/gui/AddressBookModel.cpp.o"
+  "/home/levi/Desktop/Test/cryptonotewallet/src/gui/AnimatedLabel.cpp" "/home/levi/Desktop/Test/cryptonotewallet/build/CMakeFiles/easycoin.dir/src/gui/AnimatedLabel.cpp.o"
+  "/home/levi/Desktop/Test/cryptonotewallet/src/gui/ChangePasswordDialog.cpp" "/home/levi/Desktop/Test/cryptonotewallet/build/CMakeFiles/easycoin.dir/src/gui/ChangePasswordDialog.cpp.o"
+  "/home/levi/Desktop/Test/cryptonotewallet/src/gui/ExitWidget.cpp" "/home/levi/Desktop/Test/cryptonotewallet/build/CMakeFiles/easycoin.dir/src/gui/ExitWidget.cpp.o"
+  "/home/levi/Desktop/Test/cryptonotewallet/src/gui/MainWindow.cpp" "/home/levi/Desktop/Test/cryptonotewallet/build/CMakeFiles/easycoin.dir/src/gui/MainWindow.cpp.o"
+  "/home/levi/Desktop/Test/cryptonotewallet/src/gui/NewAddressDialog.cpp" "/home/levi/Desktop/Test/cryptonotewallet/build/CMakeFiles/easycoin.dir/src/gui/NewAddressDialog.cpp.o"
+  "/home/levi/Desktop/Test/cryptonotewallet/src/gui/NewPasswordDialog.cpp" "/home/levi/Desktop/Test/cryptonotewallet/build/CMakeFiles/easycoin.dir/src/gui/NewPasswordDialog.cpp.o"
+  "/home/levi/Desktop/Test/cryptonotewallet/src/gui/OverviewFrame.cpp" "/home/levi/Desktop/Test/cryptonotewallet/build/CMakeFiles/easycoin.dir/src/gui/OverviewFrame.cpp.o"
+  "/home/levi/Desktop/Test/cryptonotewallet/src/gui/PasswordDialog.cpp" "/home/levi/Desktop/Test/cryptonotewallet/build/CMakeFiles/easycoin.dir/src/gui/PasswordDialog.cpp.o"
+  "/home/levi/Desktop/Test/cryptonotewallet/src/gui/ReceiveFrame.cpp" "/home/levi/Desktop/Test/cryptonotewallet/build/CMakeFiles/easycoin.dir/src/gui/ReceiveFrame.cpp.o"
+  "/home/levi/Desktop/Test/cryptonotewallet/src/gui/RecentTransactionsModel.cpp" "/home/levi/Desktop/Test/cryptonotewallet/build/CMakeFiles/easycoin.dir/src/gui/RecentTransactionsModel.cpp.o"
+  "/home/levi/Desktop/Test/cryptonotewallet/src/gui/SendFrame.cpp" "/home/levi/Desktop/Test/cryptonotewallet/build/CMakeFiles/easycoin.dir/src/gui/SendFrame.cpp.o"
+  "/home/levi/Desktop/Test/cryptonotewallet/src/gui/SortedTransactionsModel.cpp" "/home/levi/Desktop/Test/cryptonotewallet/build/CMakeFiles/easycoin.dir/src/gui/SortedTransactionsModel.cpp.o"
+  "/home/levi/Desktop/Test/cryptonotewallet/src/gui/TransactionDetailsDialog.cpp" "/home/levi/Desktop/Test/cryptonotewallet/build/CMakeFiles/easycoin.dir/src/gui/TransactionDetailsDialog.cpp.o"
+  "/home/levi/Desktop/Test/cryptonotewallet/src/gui/TransactionFrame.cpp" "/home/levi/Desktop/Test/cryptonotewallet/build/CMakeFiles/easycoin.dir/src/gui/TransactionFrame.cpp.o"
+  "/home/levi/Desktop/Test/cryptonotewallet/src/gui/TransactionsFrame.cpp" "/home/levi/Desktop/Test/cryptonotewallet/build/CMakeFiles/easycoin.dir/src/gui/TransactionsFrame.cpp.o"
+  "/home/levi/Desktop/Test/cryptonotewallet/src/gui/TransactionsListModel.cpp" "/home/levi/Desktop/Test/cryptonotewallet/build/CMakeFiles/easycoin.dir/src/gui/TransactionsListModel.cpp.o"
+  "/home/levi/Desktop/Test/cryptonotewallet/src/gui/TransactionsModel.cpp" "/home/levi/Desktop/Test/cryptonotewallet/build/CMakeFiles/easycoin.dir/src/gui/TransactionsModel.cpp.o"
+  "/home/levi/Desktop/Test/cryptonotewallet/src/gui/TransferFrame.cpp" "/home/levi/Desktop/Test/cryptonotewallet/build/CMakeFiles/easycoin.dir/src/gui/TransferFrame.cpp.o"
+  "/home/levi/Desktop/Test/cryptonotewallet/src/main.cpp" "/home/levi/Desktop/Test/cryptonotewallet/build/CMakeFiles/easycoin.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GIT_REVISION=\"a42d92e\""
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NO_DEBUG"
+  "QT_WIDGETS_LIB"
+  "_GNU_SOURCE"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "../src"
+  "../cryptonote/external"
+  "../cryptonote/include"
+  "../cryptonote/src"
+  "../cryptonote/src/Platform/Linux"
+  "/usr/include/x86_64-linux-gnu/qt5"
+  "/usr/include/x86_64-linux-gnu/qt5/QtWidgets"
+  "/usr/include/x86_64-linux-gnu/qt5/QtGui"
+  "/usr/include/x86_64-linux-gnu/qt5/QtCore"
+  "/usr/lib/x86_64-linux-gnu/qt5/mkspecs/linux-g++-64"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/levi/Desktop/Test/cryptonotewallet/build/CMakeFiles/cryptonote.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
